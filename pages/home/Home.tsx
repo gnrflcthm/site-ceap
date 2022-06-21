c22375ff00e23f88bcb1132c21ea3f645d33c314
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-06-21 10:53:24 +0800

    separated homepage to its own folder

--- pages/home/Home.tsx
+++ pages/home/Home.tsx
@@ -10,17 +10,17 @@ import {
     NewsHighlights,
     NewsHighlight,
     MediaSlider,
-} from "../components/Home";
+} from "../../components/Home";
 
-import logo from "../public/logo.png";
-import i1 from "../assets/2.jpg";
+import logo from "../../public/logo.png";
+import i1 from "../../assets/2.jpg";
 
-import i2 from "../assets/1.jpg";
-import i3 from "../assets/3.jpg";
-import i4 from "../assets/4.jpg";
-import i5 from "../assets/5.jpg";
-import i6 from "../assets/6.jpg";
-import PartnersSlider from "../components/Home/PartnersSlider";
+import i2 from "../../assets/1.jpg";
+import i3 from "../../assets/3.jpg";
+import i4 from "../../assets/4.jpg";
+import i5 from "../../assets/5.jpg";
+import i6 from "../../assets/6.jpg";
+import PartnersSlider from "../../components/Home/PartnersSlider";
 
 const images = [i2, i3, i4, i5, i6];
 
