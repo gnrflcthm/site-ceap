c364896bed842d8ada315648f16a35e14c9efaad
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-10-12 12:29:43 +0800

    fixed school id not being added to new ms user on accept

--- pages/api/member/accept.ts
+++ pages/api/member/accept.ts
@@ -7,85 +7,87 @@ import { AccountType } from "@prisma/client";
 import { sendAcceptEmail } from "@util/email";
 import { randomBytes } from "crypto";
 
-export default authenticatedHandler([AccountType.MS_ADMIN]).post(async (req, res) => {
-    const { id } = req.body;
+export default authenticatedHandler([AccountType.MS_ADMIN]).post(
+    async (req, res) => {
+        const { id } = req.body;
 
-    const auth = getAuth();
+        const auth = getAuth();
 
-    if (!id) {
-        res.statusMessage = "Missing Required Fields";
-        res.status(418);
-        res.end();
-        return;
-    }
+        if (!id) {
+            res.statusMessage = "Missing Required Fields";
+            res.status(418);
+            res.end();
+            return;
+        }
 
-    const user = await prisma.userRegistration.findFirst({
-        where: {
-            id,
-        },
-    });
-
-    if (!user) {
-        res.statusMessage = "User Does Not Exist";
-        res.status(418);
-        res.end();
-        return;
-    }
-
-    // TODO: Fix Model Field Name Inconsistencies
-
-    try {
-        const tempPassword = randomBytes(10).toString("hex");
-
-        const { uid, displayName } = await auth.createUser({
-            displayName: `${user?.firstName} ${user?.lastName}`,
-            email: user?.email,
-            password: tempPassword,
-        });
-
-        await auth.setCustomUserClaims(uid, {
-            role: AccountType.MS_USER,
-        });
-
-        const {
-            firstName,
-            lastName,
-            email,
-            memberSchoolId,
-            mobileNumber,
-            middleName,
-        } = user;
-
-        const newUser = await prisma.user.create({
-            data: {
-                ...{
-                    email: email,
-                    firstName,
-                    lastName,
-                    middleName,
-                    memberSchoolId,
-                    mobileNumber,
-                },
-                authId: uid,
-                accountType: AccountType.MS_USER,
-                displayName: displayName || "",
-            },
-        });
-
-        await prisma.userRegistration.delete({
+        const user = await prisma.userRegistration.findFirst({
             where: {
                 id,
             },
         });
 
-        await sendAcceptEmail(newUser, tempPassword);
+        if (!user) {
+            res.statusMessage = "User Does Not Exist";
+            res.status(418);
+            res.end();
+            return;
+        }
 
-        res.statusMessage = "User Created Successfully.";
-        res.status(200);
-    } catch (error) {
-        console.log(error);
-        res.statusMessage = "Error in adding user.";
-        res.status(500);
+        // TODO: Fix Model Field Name Inconsistencies
+
+        try {
+            const tempPassword = randomBytes(10).toString("hex");
+
+            const { uid, displayName } = await auth.createUser({
+                displayName: `${user?.firstName} ${user?.lastName}`,
+                email: user?.email,
+                password: tempPassword,
+            });
+
+            await auth.setCustomUserClaims(uid, {
+                role: AccountType.MS_USER,
+            });
+
+            const {
+                firstName,
+                lastName,
+                email,
+                memberSchoolId,
+                schoolId,
+                mobileNumber,
+                middleName,
+            } = user;
+
+            const newUser = await prisma.user.create({
+                data: {
+                    email: email,
+                    firstName,
+                    lastName,
+                    middleName,
+                    schoolId,
+                    memberSchoolId,
+                    mobileNumber,
+                    authId: uid,
+                    accountType: AccountType.MS_USER,
+                    displayName: displayName || "",
+                },
+            });
+
+            await prisma.userRegistration.delete({
+                where: {
+                    id,
+                },
+            });
+
+            await sendAcceptEmail(newUser, tempPassword);
+
+            res.statusMessage = "User Created Successfully.";
+            res.status(200);
+        } catch (error) {
+            console.log(error);
+            res.statusMessage = "Error in adding user.";
+            res.status(500);
+        }
+        res.end();
     }
-    res.end();
-});
+);
