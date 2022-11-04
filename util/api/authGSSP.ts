5a1a98febaf42db15b96d3fe2500eb6516109bae
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-11-04 09:46:36 +0800

    changed imports

--- util/api/authGSSP.ts
+++ util/api/authGSSP.ts
@@ -2,7 +2,7 @@ import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { getAuth } from "firebase-admin/auth";
 import "../../firebase/admin";
 import axios from "axios";
-import { AccountType } from "@db/models";
+import { AccountType } from "@util/Enums";
 
 export interface GetServerSidePropsContextWithUser
     extends GetServerSidePropsContext {
