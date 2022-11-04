c25c5eac5139e141365f448663dbd7eb1cba4508
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-11-04 09:45:35 +0800

    changed modal header to have close option

--- components/Modal/ModalHeader.tsx
+++ components/Modal/ModalHeader.tsx
@@ -2,7 +2,7 @@ import { Flex, Heading, Box } from "@chakra-ui/react";
 import { FC } from "react";
 import { FaTimes } from "react-icons/fa";
 
-const ModalHeader: FC<{ title: string; onDismiss: Function }> = ({
+const ModalHeader: FC<{ title: string; onDismiss?: Function }> = ({
     title,
     onDismiss,
 }) => {
@@ -17,16 +17,18 @@ const ModalHeader: FC<{ title: string; onDismiss: Function }> = ({
             <Heading fontSize={"2xl"} color={"neutralizerLight"}>
                 {title}
             </Heading>
-            <Box
-                rounded={"md"}
-                cursor={"pointer"}
-                p={"2"}
-                _hover={{ bg: "whiteAlpha.200" }}
-                onClick={() => onDismiss()}
-                color={"neutralizerLight"}
-            >
-                <Box as={FaTimes} color={"neutralizerLight"} />
-            </Box>
+            {onDismiss && (
+                <Box
+                    rounded={"md"}
+                    cursor={"pointer"}
+                    p={"2"}
+                    _hover={{ bg: "whiteAlpha.200" }}
+                    onClick={() => onDismiss()}
+                    color={"neutralizerLight"}
+                >
+                    <Box as={FaTimes} color={"neutralizerLight"} />
+                </Box>
+            )}
         </Flex>
     );
 };
