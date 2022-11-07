c612878a9c0d8548e5e5b207e729988ca0dfbe2f
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-11-07 19:08:04 +0800

    added and modified api routes for handling retrieving data from folders

--- pages/api/resource/folders/[id]/folders.ts
+++ pages/api/resource/folders/[id]/folders.ts
@@ -3,14 +3,14 @@ import handler from "@util/api/handler";
 
 export default handler().get(async (req, res) => {
     const { id } = req.query;
+
     try {
         await connectDB();
-        const folder = await Folder.findById(id).populate("root", ["id"]);
-        res.status(200).json(folder);
+        const folders = await Folder.find({ root: id }).populate("root").exec();
+        res.status(200).json(folders);
     } catch (err) {
-        console.log(err);
-        res.statusMessage = "Error in retrieving folder information";
-        res.status(400);
+        res.statusMessage = "Error in retrieving folder data.";
+        res.status(500).json([]);
     }
     res.end();
 });
--- pages/api/resource/folders/[id]/resources.ts
+++ pages/api/resource/folders/[id]/resources.ts
@@ -1,6 +1,6 @@
 import { connectDB, Resource, User } from "@db/index";
 import authenticatedHandler from "@util/api/authenticatedHandler";
-import { AccountType, FileAccessibility } from "@util/Enums";
+import { AccountType, FileAccessibility, ResourceStatus } from "@util/Enums";
 
 export default authenticatedHandler([], true).get(async (req, res) => {
     const { id } = req.query;
@@ -8,24 +8,32 @@ export default authenticatedHandler([], true).get(async (req, res) => {
     await connectDB();
     if (req.isLoggedIn) {
         const user = await User.findOne({ authId: req.uid });
-        let accessibility = [
+        const accessibility: FileAccessibility[] = [
             FileAccessibility.PUBLIC,
             FileAccessibility.PRIVATE,
         ];
+        const status: ResourceStatus[] = [ResourceStatus.APPROVED];
+
         if (
             [AccountType.CEAP_ADMIN, AccountType.CEAP_SUPER_ADMIN].includes(
                 user?.accountType || AccountType.MS_USER
             )
         ) {
             accessibility.push(FileAccessibility.HIDDEN);
+            status.push(ResourceStatus.ARCHIVED);
         }
 
-        const resources = await Resource.find({ folder: id, accessibility });
+        const resources = await Resource.find({
+            folder: id,
+            accessibility,
+            status,
+        });
         res.status(200).json(resources.map((resource) => resource.toJSON()));
     } else {
         const resources = await Resource.find({
             folder: id,
             accessibility: FileAccessibility.PUBLIC,
+            status: ResourceStatus.APPROVED,
         });
         res.status(200).json(resources.map((resource) => resource.toJSON()));
     }
