3751a8d5bee6dcd3f2c7db54ce5ed302b93027e3
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-11-10 16:37:55 +0800

    implemented archives page

---
+++ components/Archive/ArchiveItem.tsx
@@ -0,0 +1,197 @@
+import {
+    Td,
+    Tr,
+    Text,
+    Button,
+    useDisclosure,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Box,
+    useToast,
+    CircularProgress,
+    Center,
+} from "@chakra-ui/react";
+import ConfirmationModal from "@components/ConfirmationModal";
+import axios from "axios";
+import { AnimatePresence, motion } from "framer-motion";
+import dynamic from "next/dynamic";
+import { ArchiveType } from "pages/ceap/archive";
+import { FC, useState } from "react";
+import { FaCog, FaEllipsisV, FaPencilAlt, FaTrash } from "react-icons/fa";
+
+const UserInfoModal = dynamic(() => import("@components/Modal/UserInfoModal"));
+const EditResourceModal = dynamic(
+    () => import("@components/Resources/EditResourceModal")
+);
+
+const ArchiveItem: FC<{
+    resource: ArchiveType;
+}> = ({ resource }) => {
+    const textFontSize = { base: "sm", md: "md" };
+    const toast = useToast();
+
+    const {
+        isOpen: showUser,
+        onOpen: openUser,
+        onClose: closeUser,
+    } = useDisclosure();
+
+    const {
+        isOpen: showEdit,
+        onOpen: openEdit,
+        onClose: closeEdit,
+    } = useDisclosure();
+
+    const {
+        isOpen: showDelete,
+        onOpen: openDelete,
+        onClose: closeDelete,
+    } = useDisclosure();
+
+    const [processing, setProcessing] = useState<boolean>(false);
+
+    const deleteItem = () => {
+        axios
+            .delete(`/api/resource/a/${resource.id}`)
+            .then(() =>
+                toast({
+                    title: "Successfully Deleted Resource",
+                    status: "success",
+                })
+            )
+            .catch((err) =>
+                toast({ title: "Error In Deleting Resource", status: "error" })
+            )
+            .finally(() => {
+                closeDelete();
+            });
+    };
+
+    const download = async () => {
+        try {
+            setProcessing(true);
+            const { data } = await axios.get<{ downloadLink: string }>(
+                `/api/resource/a/download/${resource.id}`
+            );
+
+            const anchor = document.createElement("a");
+            anchor.setAttribute("href", data.downloadLink);
+            anchor.setAttribute("download", resource.filename);
+
+            anchor.click();
+        } catch (err) {
+            console.log(err);
+            toast({
+                status: "error",
+                title: "Error In Retrieving Resource",
+            });
+        } finally {
+            setProcessing(false);
+        }
+    };
+
+    return (
+        <>
+            <Tr
+                as={motion.tr}
+                p={0}
+                _hover={{
+                    bg: "blackAlpha.50",
+                }}
+            >
+                <Td px={"4"} py={"2"}>
+                    <Text fontSize={textFontSize}>
+                        {new Date(resource.dateAdded).toLocaleString()}
+                    </Text>
+                </Td>
+                <Td px={"4"} py={"2"}>
+                    <Button
+                        variant={"link"}
+                        onClick={() => download()}
+                        fontSize={textFontSize}
+                    >
+                        {resource.filename}
+                    </Button>
+                </Td>
+                <Td px={"4"} py={"2"}>
+                    <Text fontSize={textFontSize}>{resource.fileType}</Text>
+                </Td>
+                <Td px={"4"} py={"2"}>
+                    <Button
+                        variant={"link"}
+                        onClick={() => openUser()}
+                        fontSize={textFontSize}
+                    >
+                        {resource.uploadedBy.displayName}
+                    </Button>
+                </Td>
+                <Td px={"4"} py={"2"}>
+                    <Td px={"4"} py={"2"}>
+                        {processing ? (
+                            <Center>
+                                <CircularProgress
+                                    isIndeterminate
+                                    size={6}
+                                    color={"secondary"}
+                                />
+                            </Center>
+                        ) : (
+                            <Menu>
+                                <MenuButton w={"full"}>
+                                    <Box
+                                        as={FaEllipsisV}
+                                        m={"auto"}
+                                        cursor={"pointer"}
+                                    />
+                                </MenuButton>
+                                <MenuList>
+                                    <MenuItem onClick={() => openEdit()}>
+                                        <Box as={FaCog} mr={"2"} />
+                                        <Text fontSize={"md"} lineHeight={"0"}>
+                                            Manage
+                                        </Text>
+                                    </MenuItem>
+                                    <MenuItem onClick={() => openDelete()}>
+                                        <Box as={FaTrash} mr={"2"} />
+                                        <Text fontSize={"md"} lineHeight={"0"}>
+                                            Delete
+                                        </Text>
+                                    </MenuItem>
+                                </MenuList>
+                            </Menu>
+                        )}
+                    </Td>
+                </Td>
+            </Tr>
+            <AnimatePresence>
+                {showUser && (
+                    <UserInfoModal
+                        userId={resource.uploadedBy.id}
+                        onDismiss={() => closeUser()}
+                    />
+                )}
+                {showEdit && (
+                    <EditResourceModal
+                        resource={resource}
+                        onDismiss={() => closeEdit()}
+                    />
+                )}
+                {showDelete && (
+                    <ConfirmationModal
+                        title={"Delete Resource"}
+                        acceptText={"Delete"}
+                        rejectText={"Cancel"}
+                        prompt={`You are about to delete <b>${resource.filename}</b>. Do you want to proceed?`}
+                        onReject={() => closeDelete()}
+                        onAccept={() => deleteItem()}
+                        willProcessOnAccept={true}
+                    />
+                )}
+            </AnimatePresence>
+        </>
+    );
+};
+
+export default ArchiveItem;
---
+++ pages/ceap/archive.tsx
@@ -0,0 +1,170 @@
+import { PageWithLayout } from "pages/_app";
+import Layout from "@components/Layout";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import dynamic from "next/dynamic";
+import AuthGetServerSideProps, {
+    GetServerSidePropsContextWithUser,
+} from "@util/api/authGSSP";
+import Head from "next/head";
+import TopPanel from "@components/TopPanel";
+import {
+    Text,
+    Center,
+    CircularProgress,
+    Flex,
+    Table,
+    TableContainer,
+    Tbody,
+    Thead,
+    Tr,
+    Td,
+} from "@chakra-ui/react";
+import { AccountType, ResourceStatus } from "@util/Enums";
+import { IResourceSchema, Resource } from "@db/models";
+import { connectDB } from "@db/index";
+import TableHeader from "@components/TableHeader";
+import { useData } from "@util/hooks/useData";
+import { FilterQuery } from "mongoose";
+import SearchBar from "@components/SearchBar";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const ArchiveItem = dynamic(() => import("@components/Archive/ArchiveItem"));
+
+const Archives: PageWithLayout<
+    InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ resources, q }) => {
+    const { data, isLoading } = useData<ArchiveType[]>("", resources);
+    const router = useRouter();
+
+    const [query, setQuery] = useState<string>(q || "");
+
+    const search = () => {
+        router.push(`/ceap/archive?q=${query}`);
+    };
+
+    return (
+        <>
+            <Head>
+                <title>Archived Resources</title>
+            </Head>
+            <TopPanel title={"Archived Resources"} />
+            <Flex justify={"flex-end"} align={"center"} p={"4"}>
+                <SearchBar
+                    query={query}
+                    setQuery={setQuery}
+                    onSearch={() => search()}
+                    inputColor={"neutralizerDark"}
+                />
+            </Flex>
+            <TableContainer>
+                <Table>
+                    <Thead bg={"gray.100"} position={"sticky"} top={"0"}>
+                        <Tr>
+                            <TableHeader heading={"Date Uploaded"} sortable />
+                            <TableHeader heading={"File Name"} />
+                            <TableHeader heading={"file type"} />
+                            <TableHeader heading={"uploaded by"} />
+                            <TableHeader heading={""} />
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {data && data.length > 0 ? (
+                            data.map((resource) => (
+                                <ArchiveItem
+                                    key={resource.id}
+                                    resource={resource}
+                                />
+                            ))
+                        ) : (
+                            <Tr>
+                                <Td colSpan={4}>
+                                    <Center>
+                                        {isLoading ? (
+                                            <CircularProgress isIndeterminate size={8} color={'secondary'} />
+                                        ) : (
+                                            <Text>No Resources Found.</Text>
+                                        )}
+                                    </Center>
+                                </Td>
+                            </Tr>
+                        )}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </>
+    );
+};
+
+export type ArchiveType = IResourceSchema & {
+    id: string;
+    dateAdded: string;
+    uploadedBy: { id: string; displayName: string };
+    folder: string;
+};
+
+export const getServerSideProps: GetServerSideProps<{
+    resources?: ArchiveType[];
+    q?: string;
+}> = AuthGetServerSideProps(
+    async ({ req, query }: GetServerSidePropsContextWithUser) => {
+        let q: string | undefined = undefined;
+        let p: number | undefined = undefined;
+
+        if (query.p && typeof query.p === "string") {
+            try {
+                p = parseInt(query.p) - 1;
+                if (p < 1) {
+                    return {
+                        notFound: true,
+                    };
+                }
+            } catch {
+                return {
+                    notFound: true,
+                };
+            }
+        } else {
+            p = 0;
+        }
+
+        if (query.q) {
+            if (Array.isArray(query.q)) {
+                q = query.q[0];
+            } else {
+                q = query.q;
+            }
+        }
+
+        await connectDB();
+
+        const tempQuery: FilterQuery<IResourceSchema> = {
+            status: ResourceStatus.ARCHIVED,
+        };
+
+        if (q) {
+            tempQuery["filename"] = { $regex: `.*${q}.*`, $options: "i" };
+        }
+
+        const resources = await Resource.find(tempQuery)
+            .populate("uploadedBy", ["id", "displayName"])
+            .skip(p * 15)
+            .limit(15);
+
+        return {
+            props: {
+                resources: resources.map((resource) => ({
+                    ...resource.toJSON(),
+                    dateAdded: resource.dateAdded.toLocaleString(),
+                    folder: resource.folder?.toHexString(),
+                })),
+                q: q || null,
+            },
+        };
+    },
+    [AccountType.CEAP_ADMIN, AccountType.CEAP_SUPER_ADMIN]
+);
+
+Archives.PageLayout = Layout;
+
+export default Archives;
