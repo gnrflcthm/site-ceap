776740e09ca6168130ba8b37670136d9b8a31030
Gian Thiam <70900049+gnrflcthm@users.noreply.github.com>
2022-11-30 00:17:46 +0800

    updated profile page

--- components/Profile/UserInfo.tsx
+++ components/Profile/UserInfo.tsx
@@ -1,85 +1,24 @@
-import { FC, useState, MouseEventHandler, useContext } from "react";
+import { FC } from "react";
 
-import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
-
-import { FaPencilAlt } from "react-icons/fa";
-import CoreInput from "@components/CoreInput";
-import { ProfileModeContext } from "pages/profile";
+import { Flex, Heading, Text } from "@chakra-ui/react";
 
 const UserInfo: FC<{
     label?: string;
     value?: string;
     setValue?: Function;
     isEditable?: boolean;
-    isHidden?: boolean;
-    placeholder?: string;
-    onClick?: Function;
-}> = ({
-    label,
-    value,
-    setValue = () => {},
-    isEditable = false,
-    isHidden = false,
-    placeholder,
-    onClick,
-}) => {
-    const [updating, setUpdating] = useContext(ProfileModeContext);
-
-    const handleClick = () => {
-        if (!isEditable) return;
-        setUpdating(true);
-    };
-
+}> = ({ label, value, isEditable = false }) => {
     return (
-        <>
-            <Flex
-                justify={"space-between"}
-                align={"center"}
-                w={"full"}
-                onClick={() => {
-                    if (onClick) {
-                        onClick();
-                        return;
-                    }
-                    handleClick();
-                }}
-            >
-                <Heading fontSize={{ base: "md", lg: "xl" }} w={"fit-content"}>
-                    {label}
-                </Heading>
-                <Flex
-                    align={"center"}
-                    justify={"space-between"}
-                    _hover={{
-                        textDecor:
-                            isEditable && !updating ? "underline" : "none",
-                    }}
-                    cursor={isEditable ? "pointer" : "initial"}
-                >
-                    {updating && isEditable && !onClick ? (
-                        <CoreInput value={value} setValue={setValue} />
-                    ) : (
-                        <>
-                            {isEditable && (
-                                <Box
-                                    as={FaPencilAlt}
-                                    color={"primary"}
-                                    fontSize={"sm"}
-                                    mr={"2"}
-                                />
-                            )}
-                            <Text
-                                color={
-                                    isEditable ? "primary" : "neutralizerDark"
-                                }
-                            >
-                                {isHidden ? placeholder : value || "N/A"}
-                            </Text>
-                        </>
-                    )}
-                </Flex>
+        <Flex justify={"space-between"} align={"center"} w={"full"}>
+            <Heading fontSize={{ base: "md", lg: "xl" }} w={"fit-content"}>
+                {label}
+            </Heading>
+            <Flex align={"center"} justify={"space-between"}>
+                <Text color={isEditable ? "primary" : "neutralizerDark"}>
+                    {value || "N/A"}
+                </Text>
             </Flex>
-        </>
+        </Flex>
     );
 };
 
